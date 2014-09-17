bf44d763f9d1d6c10f5c1ffde67f9e3e3dbcaaaf
David White <david.white@spry-soft.com>
2014-09-17 21:23:32 +0100

    Changed BBC sandbox settings into zsh function

---
+++ zsh-functions/sandbox.zsh
@@ -0,0 +1,17 @@
+function forge {
+  if [ $1 == "start" ]; then
+    echo "Starting Forge sandbox"
+    VBoxHeadless --startvm "Forge sandbox" &
+  elif [ $1 == "ssh" ]; then
+    ssh 'root@pal.sandbox.dev.bbc.co.uk'
+  fi
+}
+
+function cosmos {
+  if [ $2 == "start" ]; then
+    echo "Starting Cosmos (AWS) sandbox"
+    VBoxHeadless --startvm "Cosmos (AWS) sandbox" &
+  elif [ $2 == "ssh" ]; then
+    echo "TOOD: Add ssh method to login"
+  fi
+}
