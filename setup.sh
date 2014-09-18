c19709bffdc025ad966fa4383504e565cbbc5c0b
David White <david.white@spry-soft.com>
2014-09-18 13:23:36 +0100

    Updated setup to pull down themes

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 mkdir -p vim/colors
-cd vim/colors
 curl -O https://raw.githubusercontent.com/chriskempson/tomorrow-theme/master/vim/colors/Tomorrow-Night-Eighties.vim
-cd ../..
+mv -f Tomorrow-Night-Eighties.vim vim/colors
 
 mkdir -p iterm
-cd iterm
-curl -O https://raw.githubusercontent.com/chriskempson/tomorrow-theme/master/iTerm2/Tomorrow%20Night%20Eighties.itermcolors > TomorrowNightEighties.itermcolors
-cd ..
+curl -O https://raw.githubusercontent.com/chriskempson/tomorrow-theme/master/iTerm2/Tomorrow%20Night%20Eighties.itermcolors
+mv -f Tomorrow%20Night%20Eighties.itermcolors iterm
+
+mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
+curl -O https://raw.githubusercontent.com/chriskempson/tomorrow-theme/master/Xcode%204/Tomorrow%20Night%20Eighties.dvtcolortheme
+mv Tomorrow%20Night%20Eighties.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
 
 mkdir -p bin
